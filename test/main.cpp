811083c7401089236908bdb19bf9d13a44293d18
endail <52652357+endail@users.noreply.github.com>
2020-03-01 02:49:41 +1100

    add ref unit/cleanup

--- test/main.cpp
+++ test/main.cpp
@@ -1,42 +1,26 @@
-
 #include <iostream>
-#include "../include/HX711.h"
 #include <unistd.h>
 #include <iomanip>
-#include <math.h>
+#include "../include/HX711.h"
 
-
-//  wiringPi numbered pins
-//  ./test [DATA-PIN] [CLOCK-PIN]
-//
 int main(int argc, char** argv) {
 
     int dataPin = std::stoi(argv[1]);
     int clockPin = std::stoi(argv[2]);
+    int refUnit = std::stoi(argv[3]);
 
     HX711::HX711 hx(dataPin, clockPin);
 
-    hx.set_reference_unit(-429100);
+    hx.set_reference_unit(refUnit);
     hx.tare();
     hx.reset();
 
-    double weight;
-
     while(true) {
-
-        weight = round(hx.get_weight(1) * 1000);
-
-        if(weight == -0) {
-            weight = 0;
-        }
-
         std::cout
             << std::fixed
-            << std::setprecision(0)
-            << weight
-            << " grams"
+            << std::setprecision(15)
+            << hx.get_weight(1)
             << std::endl;
-
         sleep(1);
     }
 
