2daf5fca156bbf8b537495ec85f3533855c576ce
endail <52652357+endail@users.noreply.github.com>
2020-03-02 05:41:55 +1100

    cleanup + header

--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2020 Daniel Robertson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <iostream>
 #include <unistd.h>
 #include <iomanip>
@@ -5,29 +27,26 @@
 
 int main(int argc, char** argv) {
 
+    using namespace std;
+
     const char* err = "Usage: [DATA PIN] [CLOCK PIN] [REFERENCE UNIT]";
 
     if(argc != 4) {
-        std::cout << err << std::endl;
+        cout << err << endl;
         return -1;
     }
 
-    int dataPin = std::stoi(argv[1]);
-    int clockPin = std::stoi(argv[2]);
-    int refUnit = std::stoi(argv[3]);
+    const uint8_t dataPin = stoi(argv[1]);
+    const uint8_t clockPin = stoi(argv[2]);
+    const uint8_t refUnit = stoi(argv[3]);
 
     HX711::HX711 hx(dataPin, clockPin);
 
     hx.set_reference_unit(refUnit);
     hx.tare();
-    hx.reset();
 
     while(true) {
-        std::cout
-            << std::fixed
-            << std::setprecision(15)
-            << hx.get_weight(1)
-            << std::endl;
+        cout << fixed << setprecision(15) << hx.get_weight(1) << endl;
         sleep(1);
     }
 
