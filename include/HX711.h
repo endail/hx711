f50b2d88b65bb05630bab3282ab30ad680d01d91
endail <52652357+endail@users.noreply.github.com>
2020-03-20 17:32:22 +1100

    proper int namespace

--- include/HX711.h
+++ include/HX711.h
@@ -25,7 +25,7 @@
 #ifndef HX711_HX711_H_670BFDCD_DA15_4F8B_A15C_0F0043905889
 #define HX711_HX711_H_670BFDCD_DA15_4F8B_A15C_0F0043905889
 
-#include <stdint.h>
+#include <cstdint>
 #include <mutex>
 
 namespace HX711 {
@@ -38,53 +38,53 @@ enum Format {
 class HX711 {
 
 protected:
-    uint8_t _dataPin;
-    uint8_t _clockPin;
+    std::uint8_t _dataPin;
+    std::uint8_t _clockPin;
     std::mutex _readLock;
-    uint8_t _gain;
-    int32_t _referenceUnit;
-    int32_t _referenceUnitB;
-    int32_t _offset;
-    int32_t _offsetB;
+    std::uint8_t _gain;
+    std::int32_t _referenceUnit;
+    std::int32_t _referenceUnitB;
+    std::int32_t _offset;
+    std::int32_t _offsetB;
     Format _byteFormat;
     Format _bitFormat;
 
-    static int32_t _convertFromTwosComplement(const int32_t val);
+    static std::int32_t _convertFromTwosComplement(const std::int32_t val);
     bool _readBit() const;
-    uint8_t _readByte() const;
-    void _readRawBytes(uint8_t* bytes = nullptr);
-    int32_t _readLong();
-    double _readAverage(const uint16_t times = 3);
-    double _readMedian(const uint16_t times = 3);
-    void _setOffset(const int32_t offset);
-    void _setOffsetA(const int32_t offset);
-    void _setOffsetB(const int32_t offset);
-    int32_t _getOffset() const;
-    int32_t _getOffsetA() const;
-    int32_t _getOffsetB() const;
+    std::uint8_t _readByte() const;
+    void _readRawBytes(std::uint8_t* bytes = nullptr);
+    std::int32_t _readLong();
+    double _readAverage(const std::uint16_t times = 3);
+    double _readMedian(const std::uint16_t times = 3);
+    void _setOffset(const std::int32_t offset);
+    void _setOffsetA(const std::int32_t offset);
+    void _setOffsetB(const std::int32_t offset);
+    std::int32_t _getOffset() const;
+    std::int32_t _getOffsetA() const;
+    std::int32_t _getOffsetB() const;
 
 public:
-    HX711(const uint8_t dataPin, const uint8_t clockPin, const uint8_t gain = 128);
+    HX711(const std::uint8_t dataPin, const std::uint8_t clockPin, const std::uint8_t gain = 128);
     ~HX711();
     bool is_ready() const;
-    void set_gain(const uint8_t gain);
-    uint8_t get_gain() const;
-    double get_value(const uint16_t times = 3);
-    double get_value_A(const uint16_t times = 3);
-    double get_value_B(const uint16_t times = 3);
-    double get_weight(const uint16_t times = 3);
-    double get_weight_A(const uint16_t times = 3);
-    double get_weight_B(const uint16_t times = 3);
-    double tare(const uint16_t times = 15);
-    double tare_A(const uint16_t times = 15);
-    double tare_B(const uint16_t times = 15);
+    void set_gain(const std::uint8_t gain);
+    std::uint8_t get_gain() const;
+    double get_value(const std::uint16_t times = 3);
+    double get_value_A(const std::uint16_t times = 3);
+    double get_value_B(const std::uint16_t times = 3);
+    double get_weight(const std::uint16_t times = 3);
+    double get_weight_A(const std::uint16_t times = 3);
+    double get_weight_B(const std::uint16_t times = 3);
+    double tare(const std::uint16_t times = 15);
+    double tare_A(const std::uint16_t times = 15);
+    double tare_B(const std::uint16_t times = 15);
     void set_reading_format(const Format byteFormat = Format::MSB, const Format bitFormat = Format::MSB);
-    void set_reference_unit(const int32_t refUnit);
-    void set_reference_unit_A(const int32_t refUnit);
-    void set_reference_unit_B(const int32_t refUnit);
-    int32_t get_reference_unit() const;
-    int32_t get_reference_unit_A() const;
-    int32_t get_reference_unit_B() const;
+    void set_reference_unit(const std::int32_t refUnit);
+    void set_reference_unit_A(const std::int32_t refUnit);
+    void set_reference_unit_B(const std::int32_t refUnit);
+    std::int32_t get_reference_unit() const;
+    std::int32_t get_reference_unit_A() const;
+    std::int32_t get_reference_unit_B() const;
     void power_down();
     void power_up();
     void reset();
--- src/HX711.cpp
+++ src/HX711.cpp
@@ -21,6 +21,7 @@
 // SOFTWARE.
 
 #include "../include/HX711.h"
+#include <cstdint>
 #include <wiringPi.h>
 #include <thread>
 #include <chrono>
@@ -30,21 +31,26 @@
 
 namespace HX711 {
 
-int32_t HX711::_convertFromTwosComplement(const int32_t val) {
+std::int32_t HX711::_convertFromTwosComplement(const std::int32_t val) {
     return -(val & 0x800000) + (val & 0x7fffff);
 }
 
 bool HX711::_readBit() const {
+    //https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
+    //clock should be high for 1 microsecond
+    //then low for 1 microsecond
+    //does this require a sleep just to be sure?
+    //is pi slow that it doesn't matter?
     digitalWrite(this->_clockPin, HIGH);
     digitalWrite(this->_clockPin, LOW);
     return digitalRead(this->_dataPin) == HIGH;
 }
 
-uint8_t HX711::_readByte() const {
+std::uint8_t HX711::_readByte() const {
 
-    uint8_t val = 0;
+    std::uint8_t val = 0;
 
-    for(uint8_t i = 0; i < 8; ++i) {
+    for(std::uint8_t i = 0; i < 8; ++i) {
         if(this->_bitFormat == Format::MSB) {
             val <<= 1;
             val |= this->_readBit();
@@ -59,19 +65,20 @@ uint8_t HX711::_readByte() const {
 
 }
 
-void HX711::_readRawBytes(uint8_t* bytes) {
+void HX711::_readRawBytes(std::uint8_t* bytes) {
 
     std::unique_lock<std::mutex> lock(this->_readLock);
 
     while(!this->is_ready()) {
+        //DOUT falling edge to PD_SCK rising edge MIN 0.1 us
         std::this_thread::sleep_for(std::chrono::nanoseconds(100));
     }
 
-    const uint8_t byte1 = this->_readByte();
-    const uint8_t byte2 = this->_readByte();
-    const uint8_t byte3 = this->_readByte();
+    const std::uint8_t byte1 = this->_readByte();
+    const std::uint8_t byte2 = this->_readByte();
+    const std::uint8_t byte3 = this->_readByte();
 
-    for(uint8_t i = 0; i < this->_gain; ++i) {
+    for(std::uint8_t i = 0; i < this->_gain; ++i) {
         this->_readBit();
     }
 
@@ -94,23 +101,23 @@ void HX711::_readRawBytes(uint8_t* bytes) {
 
 }
 
-int32_t HX711::_readLong() {
+std::int32_t HX711::_readLong() {
 
-    uint8_t bytes[3];
+    std::uint8_t bytes[3];
     
     this->_readRawBytes(bytes);
 
-    const int32_t twosComp = ((bytes[0] << 16) |
+    const std::int32_t twosComp = ((bytes[0] << 16) |
                               (bytes[1] << 8)  |
                               bytes[2]);
 
-    const int32_t signedInt = _convertFromTwosComplement(twosComp);
+    const std::int32_t signedInt = _convertFromTwosComplement(twosComp);
 
     return signedInt;
 
 }
 
-double HX711::_readAverage(const uint16_t times) {
+double HX711::_readAverage(const std::uint16_t times) {
 
     if(times == 0) {
         throw std::invalid_argument("times must be greater than 0");
@@ -119,25 +126,25 @@ double HX711::_readAverage(const uint16_t times) {
         return this->_readMedian(times);
     }
 
-    std::vector<int32_t> values;
+    std::vector<std::int32_t> values;
     values.reserve(times);
 
-    for(uint16_t i = 0; i < times; ++i) {
+    for(std::uint16_t i = 0; i < times; ++i) {
         values.push_back(this->_readLong());
     }
 
     std::sort(values.begin(), values.end());
 
-    const uint16_t trimAmount = values.size() * 0.2;
-    values = std::vector<int32_t>(values.begin() + trimAmount, values.end() - trimAmount);
+    const std::uint16_t trimAmount = values.size() * 0.2;
+    values = std::vector<std::int32_t>(values.begin() + trimAmount, values.end() - trimAmount);
 
-    const int64_t sum = std::accumulate(values.begin(), values.end(), 0);
+    const std::int64_t sum = std::accumulate(values.begin(), values.end(), 0);
 
     return (double)sum / values.size();
 
 }
 
-double HX711::_readMedian(const uint16_t times) {
+double HX711::_readMedian(const std::uint16_t times) {
 
     if(times == 0) {
         throw std::invalid_argument("times must be greater than 0");
@@ -146,10 +153,10 @@ double HX711::_readMedian(const uint16_t times) {
         return (double)this->_readLong();
     }
 
-    std::vector<int32_t> values;
+    std::vector<std::int32_t> values;
     values.reserve(times);
 
-    for(uint16_t i = 0; i < times; ++i) {
+    for(std::uint16_t i = 0; i < times; ++i) {
         values.push_back(this->_readLong());
     }
 
@@ -159,39 +166,39 @@ double HX711::_readMedian(const uint16_t times) {
         return values[values.size() / 2];
     }
 
-    const uint16_t midpoint = values.size() / 2;
-    const int64_t sum = std::accumulate(values.begin() + midpoint, values.begin() + midpoint + 2, 0);
+    const std::uint16_t midpoint = values.size() / 2;
+    const std::int64_t sum = std::accumulate(values.begin() + midpoint, values.begin() + midpoint + 2, 0);
 
     return sum / 2.0;
 
 
 }
 
-void HX711::_setOffset(const int32_t offset) {
+void HX711::_setOffset(const std::int32_t offset) {
     this->_setOffsetA(offset);
 }
 
-void HX711::_setOffsetA(const int32_t offset) {
+void HX711::_setOffsetA(const std::int32_t offset) {
     this->_offset = offset;
 }
 
-void HX711::_setOffsetB(const int32_t offset) {
+void HX711::_setOffsetB(const std::int32_t offset) {
     this->_offsetB = offset;
 }
 
-int32_t HX711::_getOffset() const {
+std::int32_t HX711::_getOffset() const {
     return this->_getOffsetA();
 }
 
-int32_t HX711::_getOffsetA() const {
+std::int32_t HX711::_getOffsetA() const {
     return this->_offset;
 }
 
-int32_t HX711::_getOffsetB() const {
+std::int32_t HX711::_getOffsetB() const {
     return this->_offsetB;
 }
 
-HX711::HX711(const uint8_t dataPin, const uint8_t clockPin, const uint8_t gain) {
+HX711::HX711(const std::uint8_t dataPin, const std::uint8_t clockPin, const std::uint8_t gain) {
 
     this->_dataPin = dataPin;
     this->_clockPin = clockPin;
@@ -216,7 +223,7 @@ bool HX711::is_ready() const {
     return digitalRead(this->_dataPin) == LOW;
 }
 
-void HX711::set_gain(const uint8_t gain) {
+void HX711::set_gain(const std::uint8_t gain) {
 
     if(gain == 128) {
         this->_gain = 1;
@@ -234,7 +241,7 @@ void HX711::set_gain(const uint8_t gain) {
 
 }
 
-uint8_t HX711::get_gain() const {
+std::uint8_t HX711::get_gain() const {
 
     if(this->_gain == 1) {
         return 128;
@@ -250,45 +257,45 @@ uint8_t HX711::get_gain() const {
 
 }
 
-double HX711::get_value(const uint16_t times) {
+double HX711::get_value(const std::uint16_t times) {
     return this->get_value_A(times);
 }
 
-double HX711::get_value_A(const uint16_t times) {
+double HX711::get_value_A(const std::uint16_t times) {
     return this->_readMedian(times) - this->_getOffsetA();
 }
 
-double HX711::get_value_B(const uint16_t times) {
-    const uint8_t gain = this->get_gain();
+double HX711::get_value_B(const std::uint16_t times) {
+    const std::uint8_t gain = this->get_gain();
     this->set_gain(32);
     const double val = this->_readMedian(times) - this->_getOffsetB();
     this->set_gain(gain);
     return val;
 }
 
-double HX711::get_weight(const uint16_t times) {
+double HX711::get_weight(const std::uint16_t times) {
     return this->get_weight_A(times);
 }
 
-double HX711::get_weight_A(const uint16_t times) {
+double HX711::get_weight_A(const std::uint16_t times) {
     double val = this->get_value_A(times);
     val = val / this->_referenceUnit;
     return val;
 }
 
-double HX711::get_weight_B(const uint16_t times) {
+double HX711::get_weight_B(const std::uint16_t times) {
     double val = this->get_value_B(times);
     val = val / this->_referenceUnitB;
     return val;
 }
 
-double HX711::tare(const uint16_t times) {
+double HX711::tare(const std::uint16_t times) {
     return this->tare_A(times);
 }
 
-double HX711::tare_A(const uint16_t times) {
+double HX711::tare_A(const std::uint16_t times) {
 
-    const int32_t backupRefUnit = this->get_reference_unit_A();
+    const std::int32_t backupRefUnit = this->get_reference_unit_A();
     this->set_reference_unit_A(1);
 
     const double val = this->_readAverage(times);
@@ -300,12 +307,12 @@ double HX711::tare_A(const uint16_t times) {
 
 }
 
-double HX711::tare_B(const uint16_t times) {
+double HX711::tare_B(const std::uint16_t times) {
 
-    const int32_t backupRefUnit = this->get_reference_unit_B();
+    const std::int32_t backupRefUnit = this->get_reference_unit_B();
     this->set_reference_unit_B(1);
 
-    const uint8_t backupGain = this->get_gain();
+    const std::uint8_t backupGain = this->get_gain();
     this->set_gain(32);
 
     const double val = this->_readAverage(times);
@@ -324,11 +331,11 @@ void HX711::set_reading_format(const Format byteFormat, const Format bitFormat) 
     this->_bitFormat = bitFormat;
 }
 
-void HX711::set_reference_unit(const int32_t refUnit) {
+void HX711::set_reference_unit(const std::int32_t refUnit) {
     this->set_reference_unit_A(refUnit);
 }
 
-void HX711::set_reference_unit_A(const int32_t refUnit) {
+void HX711::set_reference_unit_A(const std::int32_t refUnit) {
 
     if(refUnit == 0) {
         throw std::invalid_argument("reference unit cannot be 0");
@@ -338,7 +345,7 @@ void HX711::set_reference_unit_A(const int32_t refUnit) {
 
 }
 
-void HX711::set_reference_unit_B(const int32_t refUnit) {
+void HX711::set_reference_unit_B(const std::int32_t refUnit) {
 
     if(refUnit == 0) {
         throw std::invalid_argument("reference unit cannot be 0");
@@ -348,15 +355,15 @@ void HX711::set_reference_unit_B(const int32_t refUnit) {
 
 }
 
-int32_t HX711::get_reference_unit() const {
+std::int32_t HX711::get_reference_unit() const {
     return this->get_reference_unit_A();
 }
 
-int32_t HX711::get_reference_unit_A() const {
+std::int32_t HX711::get_reference_unit_A() const {
     return this->_referenceUnit;
 }
 
-int32_t HX711::get_reference_unit_B() const {
+std::int32_t HX711::get_reference_unit_B() const {
     return this->_referenceUnitB;
 }
 
@@ -367,6 +374,7 @@ void HX711::power_down() {
     digitalWrite(this->_clockPin, LOW);
     digitalWrite(this->_clockPin, HIGH);
 
+    //change to 60us?
     std::this_thread::sleep_for(std::chrono::microseconds(100));
 
 }
@@ -377,6 +385,8 @@ void HX711::power_up() {
 
     digitalWrite(this->_clockPin, LOW);
 
+    //is sleep required here?
+    //if so, how long?
     std::this_thread::sleep_for(std::chrono::microseconds(100));
 
     lock.unlock();
