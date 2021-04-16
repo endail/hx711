86fad0c2f45cc826200b5184173faaf4a6da8b34
endail <52652357+endail@users.noreply.github.com>
2021-04-17 02:07:41 +1000

    language update

--- src/Calibration.cpp
+++ src/Calibration.cpp
@@ -82,14 +82,14 @@ int main(int argc, char** argv) {
     cin.ignore();
 
     //tare prompt
-    cout    << endl << "4. Remove all objects from the scale then press enter.";
+    cout    << endl << "4. Remove all objects from the scale and then press enter.";
     cin.ignore();
     cout    << endl << "Working..." << flush;
 
     zeroValue = hx->get_value(samples);
 
     //weigh prompt
-    cout    << endl << endl << "5. Place object on the scale then press enter.";
+    cout    << endl << endl << "5. Place the object on the scale and then press enter.";
     cin.ignore();
     cout    << endl << "Working..." << flush;
 
