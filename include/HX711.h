9affcb8461e9156fbcf9237235711b7087835d5b
endail <52652357+endail@users.noreply.github.com>
2021-04-28 02:00:59 +1000

    code simplification

--- include/HX711.h
+++ include/HX711.h
@@ -20,36 +20,38 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//C++ port of https://github.com/tatobari/hx711py
-
 #ifndef HX711_HX711_H_670BFDCD_DA15_4F8B_A15C_0F0043905889
 #define HX711_HX711_H_670BFDCD_DA15_4F8B_A15C_0F0043905889
 
+#include <chrono>
 #include <cstdint>
 #include <mutex>
-#include <vector>
 
 namespace HX711 {
+
+/**
+ * Datasheet
+ * https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
+ */
 
 enum class Format {
     MSB,
     LSB
 };
 
-/**
- *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
- *  pg. 4
- */
+enum class Channel {
+    A,
+    B
+};
+
+//Datasheet pg. 4
 enum class Gain {
     GAIN_128 = 0,
     GAIN_32,
     GAIN_64
 };
 
-/**
- *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
- *  pg. 4
- */
+//Datasheet pg. 4
 const std::uint8_t PULSES[3] = {
     25,
     26,
@@ -60,75 +62,58 @@ class HX711 {
 
 protected:
 
-    static const std::uint8_t _BITS_PER_BYTE = 8;
-
     /**
-     *  "Conversion period"
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5
+     * Maximum number of attempts to read bytes from the sensor
+     * before failing
      */
+    static const std::uint8_t _MAX_READ_TRIES = 3;
+    static constexpr std::chrono::microseconds _WAIT_INTERVAL = std::chrono::microseconds(1);
+
+    //Datasheet pg. 5
     static const std::uint8_t _BYTES_PER_CONVERSION_PERIOD = 3;
 
-    std::uint8_t _dataPin;
-    std::uint8_t _clockPin;
+    const int _dataPin = -1;
+    const int _clockPin = -1;
     std::mutex _readLock;
-    Gain _gain;
-    std::int32_t _referenceUnit;
-    std::int32_t _referenceUnitB;
-    std::int32_t _offset;
-    std::int32_t _offsetB;
-    Format _byteFormat;
-    Format _bitFormat;
+    Gain _gain = Gain::GAIN_128;
+    Format _bitFormat = Format::MSB;
+    Format _byteFormat = Format::MSB;
 
     static std::int32_t _convertFromTwosComplement(const std::int32_t val) noexcept;
     bool _readBit() const noexcept;
     std::uint8_t _readByte() const noexcept;
-    void _readRawBytes(std::uint8_t* bytes = nullptr) noexcept;
-    std::int32_t _readInt() noexcept;
+    void _readRawBytes(std::uint8_t* bytes = nullptr);
+    std::int32_t _readInt();
+
+    std::int32_t _getChannelAValue();
+    std::int32_t _getChannelBValue();
 
 public:
-    HX711(
-        const std::uint8_t dataPin,
-        const std::uint8_t clockPin,
-        const Gain gain = Gain::GAIN_128);
-
+    
+    HX711(const int dataPin, const int clockPin) noexcept;
     virtual ~HX711() = default;
-    std::uint8_t getDataPin() const noexcept;
-    std::uint8_t getClockPin() const noexcept;
-    bool is_ready() const noexcept;
-    void set_gain(const Gain gain) noexcept;
-    Gain get_gain() const noexcept;
-    double get_value(const std::uint16_t times = 3) noexcept;
-    double get_value_A(const std::uint16_t times = 3) noexcept;
-    double get_value_B(const std::uint16_t times = 3) noexcept;
-    double get_weight(const std::uint16_t times = 3) noexcept;
-    std::vector<double> get_weights(const std::uint16_t times = 3);
-    double get_weight_A(const std::uint16_t times = 3) noexcept;
-    double get_weight_B(const std::uint16_t times = 3) noexcept;
-    double tare(const std::uint16_t times = 15) noexcept;
-    double tare_A(const std::uint16_t times = 15) noexcept;
-    double tare_B(const std::uint16_t times = 15) noexcept;
-    void set_reading_format(
+
+    int getDataPin() const noexcept;
+    int getClockPin() const noexcept;
+
+    void setGain(const Gain gain);
+    Gain getGain() const noexcept;
+
+    void connect(
+        const Gain gain = Gain::GAIN_128,
+        const Format bitFormat = Format::MSB,
+        const Format byteFormat = Format::MSB);
+    
+    bool isReady() const noexcept;
+
+    std::int32_t getValue(const Channel c = Channel::A);
+
+    void setReadFormat(
         const Format bitFormat = Format::MSB,
         const Format byteFormat = Format::MSB) noexcept;
-    void set_reference_unit(const std::int32_t refUnit);
-    void set_reference_unit_A(const std::int32_t refUnit);
-    void set_reference_unit_B(const std::int32_t refUnit);
-    std::int32_t get_reference_unit() const noexcept;
-    std::int32_t get_reference_unit_A() const noexcept;
-    std::int32_t get_reference_unit_B() const noexcept;
-    void setOffset(const std::int32_t offset) noexcept;
-    void setOffsetA(const std::int32_t offset) noexcept;
-    void setOffsetB(const std::int32_t offset) noexcept;
-    std::int32_t getOffset() const noexcept;
-    std::int32_t getOffsetA() const noexcept;
-    std::int32_t getOffsetB() const noexcept;
-    std::vector<std::int32_t> readValues(const std::uint16_t times = 3);
-    double readAverageValue(const std::uint16_t times = 3);
-    double readMedianValue(const std::uint16_t times = 3);
-    void power_down() noexcept;
-    void power_up() noexcept;
-    void reset() noexcept;
+
+    void powerDown() noexcept;
+    void powerUp();
 
 };
 };
--- src/HX711.cpp
+++ src/HX711.cpp
@@ -21,12 +21,11 @@
 // SOFTWARE.
 
 #include "../include/HX711.h"
+#include "../include/TimeoutException.h"
 #include <cstdint>
-#include <wiringPi.h>
-#include <vector>
-#include <algorithm>
-#include <numeric>
 #include <cstring>
+#include <thread>
+#include <wiringPi.h>
 
 namespace HX711 {
 
@@ -51,9 +50,9 @@ bool HX711::_readBit() const noexcept {
      *  Solution: stick with 1us. It seems to work fine.
      */
     ::digitalWrite(this->_clockPin, HIGH);
-    ::delayMicroseconds(1);
+    std::this_thread::sleep_for(std::chrono::microseconds(1));
     ::digitalWrite(this->_clockPin, LOW);
-    ::delayMicroseconds(1);
+    std::this_thread::sleep_for(std::chrono::microseconds(1));
 
     return ::digitalRead(this->_dataPin) == HIGH;
 
@@ -78,36 +77,41 @@ std::uint8_t HX711::_readByte() const noexcept {
 
 }
 
-void HX711::_readRawBytes(std::uint8_t* bytes) noexcept {
+void HX711::_readRawBytes(std::uint8_t* bytes) {
 
     std::unique_lock<std::mutex> lock(this->_readLock);
 
     /**
      *  Bytes are ready to be read from the HX711 when DOUT goes low. Therefore,
      *  wait until this occurs.
-     * 
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5
-     * 
-     *  ISSUE: this is essentially an infinite-loop waiting on a GPIO pin. This
-     *  may be problematic.
+     *  Datasheet pg. 5
      */
-    while(!this->is_ready());
+
+    uint8_t tries = 0;
+
+    do {
+
+        if(this->isReady()) {
+            break;
+        }
+
+        if(++tries < _MAX_READ_TRIES) {
+            std::this_thread::sleep_for(_WAIT_INTERVAL);
+        }
+        else {
+            throw TimeoutException("timed out while trying to read bytes from HX711");
+        }
+
+    }
+    while(true);
 
     /**
      *  When DOUT goes low, there is a minimum of 0.1us until the clock pin
      *  can go high. T1 in Fig.2.
-     * 
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5
-     * 
-     *  Problem 1: because we prefer wiringPi's timing functions, we cannot
-     *  wait less than 1us.
-     * 
-     *  Solution: wait for 1us. This is 10x longer than necessary, but it
-     *  does allow sufficient time.
+     *  Datasheet pg. 5
+     *  0.1us == 100ns
      */
-    ::delayMicroseconds(1);
+    std::this_thread::sleep_for(std::chrono::nanoseconds(100));
 
     //delcare array of bytes of sufficient size
     std::uint8_t raw[_BYTES_PER_CONVERSION_PERIOD];
@@ -120,9 +124,7 @@ void HX711::_readRawBytes(std::uint8_t* bytes) noexcept {
     /**
      *  The HX711 requires a certain number of "positive clock
      *  pulses" depending on the set gain value.
-     *  
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 4
+     *  Datasheet pg. 4
      * 
      *  The expression below calculates the number of pulses
      *  after having read the three bytes above. For example,
@@ -131,8 +133,8 @@ void HX711::_readRawBytes(std::uint8_t* bytes) noexcept {
      *  additional pulse is needed.
      */
     const std::uint8_t pulsesNeeded = 
-        PULSES[static_cast<std::int32_t>(this->_gain)] -
-            _BITS_PER_BYTE * _BYTES_PER_CONVERSION_PERIOD;
+        PULSES[static_cast<int>(this->_gain)] -
+            8 * _BYTES_PER_CONVERSION_PERIOD;
 
     for(std::uint8_t i = 0; i < pulsesNeeded; ++i) {
         this->_readBit();
@@ -148,9 +150,7 @@ void HX711::_readRawBytes(std::uint8_t* bytes) noexcept {
     /**
      *  The HX711 will supply bits in big-endian format;
      *  the 0th read bit is the MSB.
-     *  
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 4
+     *  Datasheet pg. 4
      * 
      *  If this->_byteFormat indicates the HX711 is outputting
      *  bytes in LSB format, just reverse the array.
@@ -167,7 +167,7 @@ void HX711::_readRawBytes(std::uint8_t* bytes) noexcept {
 
 }
 
-std::int32_t HX711::_readInt() noexcept {
+std::int32_t HX711::_readInt() {
 
     std::uint8_t bytes[_BYTES_PER_CONVERSION_PERIOD];
     
@@ -180,291 +180,126 @@ std::int32_t HX711::_readInt() noexcept {
     const std::int32_t twosComp = ((       0 << 24) |
                                    (bytes[0] << 16) |
                                    (bytes[1] << 8)  |
-                                    bytes[2]);
+                                    bytes[2]         );
 
-    const std::int32_t signedInt = _convertFromTwosComplement(twosComp);
+    return _convertFromTwosComplement(twosComp);
 
-    return signedInt;
+}
+
+std::int32_t HX711::_getChannelAValue() {
+
+}
+
+std::int32_t HX711::_getChannelBValue() {
 
 }
 
 HX711::HX711(
-    const std::uint8_t dataPin,
-    const std::uint8_t clockPin,
-    const Gain gain)
-        :   _dataPin(dataPin),
-            _clockPin(clockPin),
-            _referenceUnit(1),
-            _offset(0),
-            _byteFormat(Format::MSB),
-            _bitFormat(Format::MSB) {
+    const int dataPin,
+    const int clockPin) :
+        _dataPin(dataPin),
+        _clockPin(clockPin) {
+            ::pinMode(this->_dataPin, INPUT);
+            ::pinMode(this->_clockPin, OUTPUT);
+}
 
-                ::pinMode(this->_dataPin, INPUT);
-                ::pinMode(this->_clockPin, OUTPUT);
+void HX711::connect(
+    const Gain gain,
+    const Format bitFormat,
+    const Format byteFormat) {
 
-                /**
-                 *  Cannot simply set this->_gain. this->set_gain()
-                 *  must be called to set the HX711 module at the
-                 *  hardware-level.
-                 */
-                this->set_gain(gain);
+        this->setReadFormat(bitFormat, byteFormat);
+
+        /**
+         *  Cannot simply set this->_gain. this->set_gain()
+         *  must be called to set the HX711 module at the
+         *  hardware-level.
+         */
+        try {
+            this->setGain(gain);
+        }
+        catch(TimeoutException& e) {
+            //if an exception is thrown here, use this as
+            //an opportunity to effectively declare that the
+            //sensor is "not connected"
+        }
 
 }
 
-std::uint8_t HX711::getDataPin() const noexcept {
-    return this->_dataPin;
-}
-
-std::uint8_t HX711::getClockPin() const noexcept {
-    return this->_clockPin;
-}
-
-bool HX711::is_ready() const noexcept {
+bool HX711::isReady() const noexcept {
     /**
      *  HX711 will be "ready" when DOUT is low.
+     *  Datasheet pg. 5
      * 
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5
+     *  This should be a one-shot test. Any follow-ups
+     *  or looping for checking if the sensor is ready
+     *  over time can/should be done by other calling code
      */
     return ::digitalRead(this->_dataPin) == LOW;
 }
 
-void HX711::set_gain(const Gain gain) noexcept {
-    this->_gain = gain;
-    ::digitalWrite(this->_clockPin, LOW);
-    this->_readRawBytes();
+std::int32_t HX711::getValue(const Channel c = Channel::A) {
+
+    if(c == Channel::A) {
+        return this->_getChannelAValue();
+    }
+    
+    //else channel B
+    return this->_getChannelBValue();
+
 }
 
-Gain HX711::get_gain() const noexcept {
+int HX711::getDataPin() const noexcept {
+    return this->_dataPin;
+}
+
+int HX711::getClockPin() const noexcept {
+    return this->_clockPin;
+}
+
+void HX711::setGain(const Gain gain) {
+
+    const Gain backup = this->_gain;
+
+    /**
+     * If the attempt to set the gain fails, it should
+     * revert back to whatever it was before
+     */
+    try {
+
+        this->_gain = gain;
+
+        //why is this here?
+        //remove if not necessary
+        ::digitalWrite(this->_clockPin, LOW);
+        
+        /**
+         * A read must take place to set the gain at the
+         * hardware level. See datasheet pg. 4 "Serial
+         * Interface".
+         */
+        this->_readRawBytes();
+        
+    }
+    catch(TimeoutException& e) {
+        this->_gain = backup;
+        throw;
+    }
+
+}
+
+Gain HX711::getGain() const noexcept {
     return this->_gain;
 }
 
-double HX711::get_value(const std::uint16_t times) noexcept {
-    return this->get_value_A(times);
+void HX711::setReadFormat(
+    const Format bitFormat,
+    const Format byteFormat) noexcept {
+        this->_bitFormat = bitFormat;
+        this->_byteFormat = byteFormat;
 }
 
-double HX711::get_value_A(const std::uint16_t times) noexcept {
-    return this->readMedianValue(times) - this->getOffsetA();
-}
-
-double HX711::get_value_B(const std::uint16_t times) noexcept {
-    const Gain gain = this->_gain;
-    this->_gain = Gain::GAIN_32;
-    const double val = this->readMedianValue(times) - this->getOffsetB();
-    this->set_gain(gain);
-    return val;
-}
-
-double HX711::get_weight(const std::uint16_t times) noexcept {
-    return this->get_weight_A(times);
-}
-
-std::vector<double> HX711::get_weights(const std::uint16_t times) {
-
-    if(times == 0) {
-        throw std::invalid_argument("times must be greater than 0");
-    }
-
-    const double refUnit = static_cast<double>(this->_referenceUnit);
-
-    std::vector<std::int32_t> rawValues = this->readValues(times);
-
-    std::vector<double> values;
-    values.reserve(times);
-
-    for(std::uint16_t i = 0; i < times; ++i) {
-        values.push_back((rawValues[i] / refUnit) - this->_offset);
-    }
-
-    return values;
-
-}
-
-double HX711::get_weight_A(const std::uint16_t times) noexcept {
-    double val = this->get_value_A(times);
-    val = val / this->_referenceUnit;
-    return val;
-}
-
-double HX711::get_weight_B(const std::uint16_t times) noexcept {
-    double val = this->get_value_B(times);
-    val = val / this->_referenceUnitB;
-    return val;
-}
-
-double HX711::tare(const std::uint16_t times) noexcept {
-    return this->tare_A(times);
-}
-
-double HX711::tare_A(const std::uint16_t times) noexcept {
-
-    const std::int32_t backupRefUnit = this->get_reference_unit_A();
-    this->set_reference_unit_A(1);
-
-    const double val = this->readMedianValue(times);
-
-    this->setOffsetA(val);
-    this->set_reference_unit_A(backupRefUnit);
-
-    return val;
-
-}
-
-double HX711::tare_B(const std::uint16_t times) noexcept {
-
-    const std::int32_t backupRefUnit = this->get_reference_unit_B();
-    this->set_reference_unit_B(1);
-
-    const Gain backupGain = this->_gain;
-    this->set_gain(Gain::GAIN_32);
-
-    const double val = this->readMedianValue(times);
-
-    this->setOffsetB(val);
-    this->set_gain(backupGain);
-    this->set_reference_unit_B(backupRefUnit);
-
-    return val;
-
-}
-
-void HX711::set_reading_format(const Format bitFormat, const Format byteFormat) noexcept {
-    this->_bitFormat = bitFormat;
-    this->_byteFormat = byteFormat;
-}
-
-void HX711::set_reference_unit(const std::int32_t refUnit) {
-    this->set_reference_unit_A(refUnit);
-}
-
-void HX711::set_reference_unit_A(const std::int32_t refUnit) {
-
-    if(refUnit == 0) {
-        throw std::invalid_argument("reference unit cannot be 0");
-    }
-
-    this->_referenceUnit = refUnit;
-
-}
-
-void HX711::set_reference_unit_B(const std::int32_t refUnit) {
-
-    if(refUnit == 0) {
-        throw std::invalid_argument("reference unit cannot be 0");
-    }
-
-    this->_referenceUnitB = refUnit;
-
-}
-
-std::int32_t HX711::get_reference_unit() const noexcept {
-    return this->get_reference_unit_A();
-}
-
-std::int32_t HX711::get_reference_unit_A() const noexcept {
-    return this->_referenceUnit;
-}
-
-std::int32_t HX711::get_reference_unit_B() const noexcept {
-    return this->_referenceUnitB;
-}
-
-void HX711::setOffset(const std::int32_t offset) noexcept {
-    this->setOffsetA(offset);
-}
-
-void HX711::setOffsetA(const std::int32_t offset) noexcept {
-    this->_offset = offset;
-}
-
-void HX711::setOffsetB(const std::int32_t offset) noexcept {
-    this->_offsetB = offset;
-}
-
-std::int32_t HX711::getOffset() const noexcept {
-    return this->getOffsetA();
-}
-
-std::int32_t HX711::getOffsetA() const noexcept {
-    return this->_offset;
-}
-
-std::int32_t HX711::getOffsetB() const noexcept {
-    return this->_offsetB;
-}
-
-std::vector<std::int32_t> HX711::readValues(const std::uint16_t times) {
-
-    if(times == 0) {
-        throw std::invalid_argument("times must be greater than 0");
-    }
-
-    std::vector<std::int32_t> values;
-    values.reserve(times);
-
-    for(std::uint16_t i = 0; i < times; ++i) {
-        values.push_back(this->_readInt());
-    }
-
-    return values;
-
-}
-
-double HX711::readAverageValue(const std::uint16_t times) {
-
-    if(times == 0) {
-        throw std::invalid_argument("times must be greater than 0");
-    }
-    
-    if(times == 1) {
-        return static_cast<double>(this->_readInt());
-    }
-
-    std::vector<std::int32_t> values = this->readValues(times);
-
-    const std::int64_t sum = std::accumulate(
-        values.begin(), values.end(), 0);
-
-    return static_cast<double>(sum) / values.size();
-
-}
-
-double HX711::readMedianValue(const std::uint16_t times) {
-
-    if(times == 0) {
-        throw std::invalid_argument("times must be greater than 0");
-    }
-    
-    if(times == 1) {
-        return (double)this->_readInt();
-    }
-
-    std::vector<std::int32_t> values = this->readValues(times);
-    
-    //https://stackoverflow.com/a/42791986/570787
-    if(values.size() % 2 == 0) {
-
-        const auto median_it1 = values.begin() + values.size() / 2 - 1;
-        const auto median_it2 = values.begin() + values.size() / 2;
-
-        std::nth_element(values.begin(), median_it1, values.end());
-        const auto e1 = *median_it1;
-
-        std::nth_element(values.begin(), median_it2, values.end());
-        const auto e2 = *median_it2;
-
-        return (e1 + e2) / 2.0;
-
-    }
-    else {
-        const auto median_it = values.begin() + values.size() / 2;
-        std::nth_element(values.begin(), median_it, values.end());
-        return static_cast<double>(*median_it);
-    }
-
-}
-
-void HX711::power_down() noexcept {
+void HX711::powerDown() noexcept {
 
     std::lock_guard<std::mutex> lock(this->_readLock);
 
@@ -475,15 +310,13 @@ void HX711::power_down() noexcept {
      *  "When PD_SCK pin changes from low to high
      *  and stays at high for longer than 60µs, HX711
      *  enters power down mode (Fig.3)."
-     * 
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5
+     *  Datasheet pg. 5
      */
-    ::delayMicroseconds(60);
+    std::this_thread::sleep_for(std::chrono::microseconds(60));
 
 }
 
-void HX711::power_up() noexcept {
+void HX711::powerUp() {
 
     std::unique_lock<std::mutex> lock(this->_readLock);
 
@@ -492,9 +325,7 @@ void HX711::power_up() noexcept {
     /**
      *  "When PD_SCK returns to low,
      *  chip will reset and enter normal operation mode"
-     * 
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5   
+     *  Datasheet pg. 5
      */
 
     lock.unlock();
@@ -503,19 +334,15 @@ void HX711::power_up() noexcept {
      *  "After a reset or power-down event, input
      *  selection is default to Channel A with a gain of
      *  128."
-     *  
-     *  https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
-     *  pg. 5 
+     *  Datasheet pg. 5
+     * 
+     *  This means the following statement to set the gain
+     *  is needed ONLY IF the current gain isn't 128
      */
     if(this->_gain != Gain::GAIN_128) {
-        this->_readRawBytes();
+        this->setGain(this->_gain);
     }
 
-}
-
-void HX711::reset() noexcept {
-    this->power_down();
-    this->power_up();
 }
 
 };
