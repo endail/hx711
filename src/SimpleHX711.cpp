20d9367b62b95d90bfad76e9b6b1895516091cad
endail <52652357+endail@users.noreply.github.com>
2021-05-11 04:45:05 +1000

    fix tabs

--- src/SimpleHX711.cpp
+++ src/SimpleHX711.cpp
@@ -33,26 +33,26 @@ namespace HX711 {
 
 std::vector<HX_VALUE> SimpleHX711::_readValues(const std::size_t times) {
 
-	std::vector<HX_VALUE> vals;
-	vals.reserve(times);
+    std::vector<HX_VALUE> vals;
+    vals.reserve(times);
 
-	for(std::size_t i = 0; i < times; ++i) {
-		vals.push_back(this->_hx->getValue(this->_ch));
-	}
+    for(std::size_t i = 0; i < times; ++i) {
+        vals.push_back(this->_hx->getValue(this->_ch));
+    }
 
-	return vals;
+    return vals;
 
 }
 
 double SimpleHX711::_median(const std::vector<HX_VALUE>* vals) {
 
-	if(vals == nullptr || vals->empty()) {
-		throw std::invalid_argument("vals is null or empty");
-	}
+    if(vals == nullptr || vals->empty()) {
+        throw std::invalid_argument("vals is null or empty");
+    }
 
-	//to calculate the median the vector needs to be modifiable
-	//hence, a copy is made
-	std::vector<HX_VALUE> copied = *vals;
+    //to calculate the median the vector needs to be modifiable
+    //hence, a copy is made
+    std::vector<HX_VALUE> copied = *vals;
 
     //https://stackoverflow.com/a/42791986/570787
     if(copied.size() % 2 == 0) {
@@ -79,9 +79,9 @@ double SimpleHX711::_median(const std::vector<HX_VALUE>* vals) {
 
 double SimpleHX711::_average(const std::vector<HX_VALUE>* vals) {
 
-	if(vals == nullptr || vals->empty()) {
-		throw std::invalid_argument("vals is null or empty");
-	}
+    if(vals == nullptr || vals->empty()) {
+        throw std::invalid_argument("vals is null or empty");
+    }
 
     const std::int64_t sum = std::accumulate(
         vals->begin(), vals->end(), 0);
@@ -94,107 +94,107 @@ SimpleHX711::SimpleHX711(const SimpleHX711& s2) noexcept {
 }
 
 SimpleHX711& SimpleHX711::operator=(const SimpleHX711& rhs) noexcept {
-	return *this;
+    return *this;
 }
 
 SimpleHX711::SimpleHX711(
-	const int dataPin,
-	const int clockPin,
-	const HX_VALUE refUnit,
-	const HX_VALUE offset) :
-		_refUnit(refUnit),
-		_offset(offset)  {
-			this->_hx = new HX711(dataPin, clockPin);
-			this->_hx->connect();
+    const int dataPin,
+    const int clockPin,
+    const HX_VALUE refUnit,
+    const HX_VALUE offset) :
+        _refUnit(refUnit),
+        _offset(offset)  {
+            this->_hx = new HX711(dataPin, clockPin);
+            this->_hx->connect();
 }
 
 SimpleHX711::~SimpleHX711() {
-	delete this->_hx;
+    delete this->_hx;
 }
 
 SimpleHX711::operator bool() const noexcept {
-	return this->_hx->isReady();
+    return this->_hx->isReady();
 }
 
 bool operator!(const SimpleHX711& hx) noexcept {
-	return !hx._hx->isReady();
+    return !hx._hx->isReady();
 }
 
 void SimpleHX711::setUnit(const Mass::Unit unit) noexcept {
-	this->_scaleUnit = unit;
+    this->_scaleUnit = unit;
 }
 
 Mass::Unit SimpleHX711::getUnit() const noexcept {
-	return this->_scaleUnit;
+    return this->_scaleUnit;
 }
 
 HX_VALUE SimpleHX711::getReferenceUnit() const noexcept {
-	return this->_refUnit;
+    return this->_refUnit;
 }
 
 void SimpleHX711::setReferenceUnit(const HX_VALUE refUnit) {
 
-	if(refUnit == 0) {
-		throw std::invalid_argument("reference unit cannot be 0");
-	}
+    if(refUnit == 0) {
+        throw std::invalid_argument("reference unit cannot be 0");
+    }
 
-	this->_refUnit = refUnit;
+    this->_refUnit = refUnit;
 
 }
 
 HX_VALUE SimpleHX711::getOffset() const noexcept {
-	return this->_offset;
+    return this->_offset;
 }
 
 void SimpleHX711::setOffset(const HX_VALUE offset) noexcept {
-	this->_offset = offset;
+    this->_offset = offset;
 }
 
 void SimpleHX711::setChannel(const Channel ch) noexcept {
-	this->_ch = ch;
+    this->_ch = ch;
 }
 
 Channel SimpleHX711::getChannel() const noexcept {
-	return this->_ch;
+    return this->_ch;
 }
 
 HX711* SimpleHX711::getBase() noexcept {
-	return this->_hx;
+    return this->_hx;
 }
 
 void SimpleHX711::tare(const ReadType r, const size_t times) {
-	const HX_VALUE backup = this->_refUnit;
-	this->setReferenceUnit(1);
-	this->_offset = static_cast<HX_VALUE>(std::round(this->read(r, times)));
-	this->setReferenceUnit(backup);
+    const HX_VALUE backup = this->_refUnit;
+    this->setReferenceUnit(1);
+    this->_offset = static_cast<HX_VALUE>(std::round(this->read(r, times)));
+    this->setReferenceUnit(backup);
 }
 
 Mass SimpleHX711::weight(const ReadType r, const size_t times) {
-	return Mass(this->read(r, times), this->_scaleUnit);
+    return Mass(this->read(r, times), this->_scaleUnit);
 }
 
 double SimpleHX711::read(const ReadType r, const std::size_t times) {
-	
-	if(times == 0) {
-		throw std::range_error("times must be at least 1");
-	}
 
-	double val;
-	
-	std::vector<HX_VALUE> vals = this->_readValues(times);
+    if(times == 0) {
+        throw std::range_error("times must be at least 1");
+    }
 
-	switch(r) {
-		case ReadType::Median:
-			val = _median(&vals);
-			break;
-		case ReadType::Average:
-			val = _average(&vals);
-			break;
-		default:
-			throw std::invalid_argument("unknown read type");
-	}
+    double val;
 
-	return (val - this->_offset) / this->_refUnit;
+    std::vector<HX_VALUE> vals = this->_readValues(times);
+
+    switch(r) {
+        case ReadType::Median:
+            val = _median(&vals);
+            break;
+        case ReadType::Average:
+            val = _average(&vals);
+            break;
+        default:
+            throw std::invalid_argument("unknown read type");
+    }
+
+    return (val - this->_offset) / this->_refUnit;
 
 }
 
