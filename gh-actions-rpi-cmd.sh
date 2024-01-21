35e42175e6c36318a001060311f284584551a998
endail <52652357+endail@users.noreply.github.com>
2024-01-21 21:54:55 +1100

    test new gh actions

--- gh-actions-rpi-cmd.sh
+++ gh-actions-rpi-cmd.sh
@@ -1,8 +1,8 @@
 
 # note that this is running on a virtual rpi
-export VIRTUAL_PI=1
-apt-get update -y && apt-get full-upgrade -y
-ldconfig
-./install-deps.sh
-make && make install
-ldconfig
+export VIRTUAL_PI=1;
+apt-get update -y && apt-get full-upgrade -y;
+./install-deps.sh;
+make;
+make install;
+ldconfig;
--- install-deps.sh
+++ install-deps.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [ `id -u` -ne 0 ]; then
-    echo "Run as root"
+    echo "Run as root";
     exit 1;
 fi
 
 # build and install liblgpio if not found
 if ! $(ldconfig -p | grep -q liblgpio); then
-    wget https://abyz.me.uk/lg/lg.zip
-    unzip lg.zip
-    cd lg
-    make
-    make install
-    cd ..
+    wget https://github.com/joan2937/lg/archive/master.zip;
+    unzip master.zip;
+    cd lg-master;
+    make;
+    make install;
+    ldconfig;
+    cd ..;
 else
-    echo "liblgpio already installed"
+    echo "liblgpio already installed";
 fi
